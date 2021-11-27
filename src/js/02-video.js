b2756e52962e87a625cb528c4ec67b7db30014bd
Oleksandr Khvaleba <alex8inbox@gmail.com>
2021-11-27 02:30:14 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,28 @@
 import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
 
-const player = new Player('handstick', {
+const player = new Player('vimeo-player', {
   id: 19231868,
-  width: 640,
+  width: 1024,
 });
-
+let currentTime=0;
 player.on('play', function () {
   console.log('played the video!');
+  // console.log(player.getCurrentTime().then);
 });
+
+const updateTime = () => {
+  player.getCurrentTime().then(function (seconds) {
+    // `seconds` indicates the current playback position of the video
+    currentTime = seconds;
+  })
+}
+// player.setCurrentTime();
+
+player.on('timeupdate', function () {
+  console.log('playing the video!');
+  _.throttle(updateTime(),2000);
+    // localStorage.setItem('currentTime', seconds);
+    // console.log(localStorage.getItem('currentTime'));
+    console.log(currentTime);
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,24 @@
-
+import throttle from 'lodash.throttle';
+import '../css/common.css';
+import '../css/03-feedback.css';
+const STORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+const formData = {};
+form.addEventListener('submit', btnFormSubmit);
+form.addEventListener('input', throttle(makeDataArray, 500));
+function makeDataArray(evt) {
+  formData[evt.target.name] = evt.target.value;
+}
+function allInputData(evt) {
+  const inputData = JSON.stringify(formData);
+  localStorage.setItem(STORAGE_KEY, inputData);
+}
+function textReturn(evt) {
+  const allDataReturn = JSON.parse(inputData);
+  localStorage.getItem(STORAGE_KEY, allDataReturn);
+}
+function btnFormSubmit(evt) {
+  evt.preventDefault();
+  evt.target.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
